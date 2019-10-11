d61f1d66a47b367dae8c7f5c57b4b7bf9bf9c90a
Steven Wichers <steven.wichers@gmail.com>
2019-10-10 23:26:35 -0700

    Add a method for clearing all site caches.

--- src/Endpoints/Action/Sites.php
+++ src/Endpoints/Action/Sites.php
@@ -169,13 +169,13 @@ class Sites extends AbstractEntityAction {
    * @param int $delaySeconds
    *   The number of seconds to wait before checks for task status.
    * @param callable|null $tickUpdate
-   *   A function to call while waiting for the backup to complete.
+   *   A function to call while waiting for the task to complete.
    *
    * @return array
    *   An array of task information.
    */
   public function backupAll(bool $waitForComplete = TRUE, int $delaySeconds = 30, callable $tickUpdate = NULL): array {
-    $backup_tasks = [];
+    $tasks = [];
 
     $site_ids = array_column($this->list()['sites'], 'id');
 
@@ -183,18 +183,60 @@ class Sites extends AbstractEntityAction {
       /** @var \swichers\Acsf\Client\Endpoints\Entity\Site $site */
       $site = $this->get($site_id);
       $label = sprintf('%s API-initiated backup', $site->details()['site']);
-      $backup_tasks[] = $site->backup(['label' => $label]);
+      $tasks[] = $site->backup(['label' => $label]);
     }
 
-    if (!empty($backup_tasks) && $waitForComplete) {
-      foreach ($backup_tasks as $task_info) {
+    if (!empty($tasks) && $waitForComplete) {
+      foreach ($tasks as $task_info) {
         $this->client
           ->getEntity('Task', intval($task_info['task_id']))
           ->wait(max(1, $delaySeconds), $tickUpdate);
       }
     }
 
-    return $backup_tasks;
+    return $tasks;
+  }
+
+  /**
+   * Initiate a cache clear for all sites.
+   *
+   * @param bool $waitForComplete
+   *   TRUE to wait for the task to complete before returning.
+   * @param int $delaySeconds
+   *   The number of seconds to wait before checks for task status.
+   * @param callable|null $tickUpdate
+   *   A function to call while waiting for the task to complete.
+   *
+   * @return array
+   *   An array of task information.
+   */
+  public function clearCaches(bool $waitForComplete = TRUE, int $delaySeconds = 30, callable $tickUpdate = NULL): array {
+
+    $tasks = [];
+
+    $site_ids = array_column($this->list()['sites'], 'id');
+
+    foreach ($site_ids as $site_id) {
+      $cache_tasks = $this->get($site_id)->clearCache();
+      if (!empty($cache_tasks['task_ids'])) {
+        $tasks[] = intval($cache_tasks['task_ids']['drupal_cache_clear']);
+        $tasks[] = intval($cache_tasks['task_ids']['varnish_cache_clear']);
+      }
+    }
+
+    array_unique($tasks);
+    array_filter($tasks);
+
+    if (!empty($tasks) && $waitForComplete) {
+      foreach ($tasks as $task_info) {
+        $this->client->getEntity('Task', intval($task_info['task_id']))->wait(
+            max(1, $delaySeconds),
+            $tickUpdate
+          );
+      }
+    }
+
+    return $tasks;
   }
 
 }
